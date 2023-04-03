cb5619b1eb05505fa77cc7570763ec9d445fb27f
elmehdiben20 <mehdibenhmidi51@gmail.com>
2023-04-03 01:31:57 +0000

    Start

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+  MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue[200],
+      appBar: AppBar(
+        title: Text('Testm'),
+        backgroundColor: Colors.blue[300],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+            'https://miro.medium.com/v2/resize:fit:10000/0*hPVHCGVgepguJt8I'),
+        ),
+      ),
+    )
+  )
+  );
+}
+
